db5ff083167a7247c9462ffe515931d8f0e5cdcd
Petrovich <yaveruvdeada@mail.ru>
2022-10-15 09:48:14 +0300

    dll+exe first commit

---
+++ ClassLibrary/ClassLibrary1/Class1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections.Concurrent;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private
+        Dictionary<string, int> Parse(string[] fContent)
+        {
+            // в мапу (словарь) запишем слова и их количество
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            //в массив запишем все символы, которые не могут войти в состав слова (возможно, сюда надо что-то еще добавить)
+            char[] delimeters = { ')', '(', ']', '[', '{', '}', ' ', ',', '.', '!', '?', ':', ';', '-', '—' };
+
+            foreach (var str in fContent)
+            {
+                // в цикле проходим по всем листам, сплитим строки файла по указанным символам.
+                // Если слова нет в словаре, то добавляем его туда. Если есть, то +1 к счетчику этого слова
+                var words = str.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    if (dict.TryAdd(word, 1) == false)
+                    {
+                        dict[word]++;
+                    }
+                }
+            }
+            return dict;
+        }
+
+        public
+        ConcurrentDictionary<string, int> ParallelParse(string[] fContent)
+        {
+            /*int concurrencyLvl = Environment.ProcessorCount;
+            int startCapacity = 1000;*/
+            ConcurrentDictionary<string, int> dict = new ConcurrentDictionary<string, int>();
+            // Насколько я понял, нет смысла в использовании объекта "Таск",
+            // так как "Parallel.ForEach" сам подрубает нужное кол-во потоков
+            /*Task kek = Task.Run(() =>
+            {*/
+                char[] delimeters = { ')', '(', ']', '[', '{', '}', ' ', ',', '.', '!', '?', ':', ';', '-', '—' };
+                Parallel.ForEach(fContent, delegate (string str)
+                {
+                    var words = str.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var word in words)
+                    {
+                        // если value++, то в функцию залетит только value!
+                        // Насколько я понял, это конкуррентно безопасное комбо методов "TryAdd" & "TryUpdate"
+                        dict.AddOrUpdate(word, 1, (key, value) => ++value);
+                    }
+                });
+            /*});
+            kek.Wait();*/
+
+            return dict;
+        }
+    }
+}
---
+++ ConsoleAppThreads/ConsoleAppThreads/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Diagnostics;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // принимаем только 1 аргумент на вход - имя файла
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Please, enter just one input file");
+            return;
+        }
+        // проверка на существование файла по указанному пути
+        string input = args[0];
+        if (File.Exists(input) == false)
+        {
+            Console.WriteLine("Your file doesnt exist! :(");
+            return;
+        }
+        // объекты для измерения эффективности (скорости выполнения) методов
+        Stopwatch norm = new Stopwatch();
+        Stopwatch parallel = new Stopwatch();
+
+        // в пердыдущей домашке я считывал строки в листы, а тут, я сдуру решил
+        // представить данные в виде массива строк. Эффективность обработки данных увеличилась!
+        var fContent = File.ReadAllLines(input);
+        var obj = new ClassLibrary1.Class1();
+
+        // манипуляции для вызова приватного метода
+        Object[] arguments = { fContent };
+        var type = typeof(ClassLibrary1.Class1);
+        var m_info = type.GetMethod("Parse", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        //Обрабатываем без параллельности
+        norm.Start();
+        var dict = (Dictionary<string, int>)m_info.Invoke(obj, arguments);
+        norm.Stop();
+
+        //Обрабатываем с параллельностью
+        parallel.Start();
+        var dictConcurrent = obj.ParallelParse(fContent)/*.ToDictionary(dict=>dict.Key, dict=>dict.Value)*/;
+        parallel.Stop();
+
+        var sort_words = dictConcurrent.OrderByDescending(_ => _.Value);
+        using (StreamWriter file = new StreamWriter("output.txt"))
+        {
+            foreach (var row in sort_words)
+            {
+                file.WriteLine($"{row.Key} {row.Value}");
+            }
+        }
+
+        Console.WriteLine("Complited!");
+        Console.WriteLine("Parallel: {0}", parallel.ElapsedMilliseconds);
+        Console.WriteLine("NORMAL: {0}", norm.ElapsedMilliseconds);
+    }
+
+    /*
+     * P.S
+     * Файл, размером 2,7 мб, содержащий томик "Войны и мира", параллельным методом обрабатывается чуть быстрее.
+     * А Файл, размером 100 мб обычным способом обрабатывался быстрее, но я думаю, это потому
+     * что он состоял из одних нулей!
+     */
+}
